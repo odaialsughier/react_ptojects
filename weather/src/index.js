3a82699019ff96c6943d56b5efebfcf10b25d53c
odai <odai@odai.com>
2018-12-25 19:34:22 +0200

    test from atom

--- weather/src/index.js
+++ weather/src/index.js
@@ -4,14 +4,14 @@ import ReactDom from 'react-dom';
 import App from './App';
 import "./css/App.css";
 
-/* To Do:  
-* - Autocomplete the city name 
+/* To Do:
+* - Autocomplete the city name
 * - multilanguge website
 */
-
+odai
 
 
 ReactDom.render(
 	<App />
 	,document.getElementById('root')
-	); 
+	);
